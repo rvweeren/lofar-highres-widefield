b316ba6a1b609284cf3e7f3505d8c7960dbf96eb
Frits Sweijen <fritssweijen@hotmail.com>
2019-11-29 11:21:55 +0100

    Restructure the whole pipeline

--- run_wsclean.sh
+++
@@ -1,5 +0,0 @@
-# Final one to store imaging weights after deciding best resolution.
-time wsclean -j 48 -mem 90 -data-column CORRECTED_DATA_LB_AMPPHASE -scale 0.20asec -no-reorder -no-update-model-required -niter 0 -weight briggs -1 -size 1024 1024 -minuvw-m 5000 -taper-gaussian 1asec -store-imaging-weights -channels-out 6 -name wsclean_taper1asec_uvcut5km_230SB_1k_ampphase *.msdpppconcat
-#time wsclean -j 48 -mem 90 -data-column CORRECTED_DATA_LB_PHASE -scale 0.20asec -no-update-model-required -niter 5000 -weight briggs -1 -size 10000 10000 -minuvw-m 30000 -taper-gaussian 1asec -baseline-averaging 200 -name wsclean_30km_1asec_taper_10SB_10k_phaseselfcal *.ms
-#time wsclean -j 48 -mem 75 -data-column CORRECTED_DATA_LB_AMPPHASE -scale 0.20asec -no-update-model-required -niter 5000 -weight briggs -1 -size 10000 10000 -minuvw-m 30000 -taper-gaussian 1asec -baseline-averaging 200 -name wsclean_30km_1asec_taper_10SB_10k_ampphaseselfcal *.ms
-
